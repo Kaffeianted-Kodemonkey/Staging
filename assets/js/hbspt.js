6a923c59b8b34d7159f42e31025f997c7ba1f7a1
Kaffeinated <kaffeinatedkodemonkey@gmail.com>
2024-08-16 16:34:38 -0600

    add plugins and update newsletter

---
+++ assets/js/hbspt.js
@@ -0,0 +1,8 @@
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+<script>
+  hbspt.forms.create({
+    region: "na1",
+    portalId: "21502915",
+    formId: "5d46f3fc-d086-4dfc-80cc-afc51ff22bff"
+  });
+</script>
